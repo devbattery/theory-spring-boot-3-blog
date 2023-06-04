07597d4b803c9c114f94a375c4337a73e53d4ab4
Sully <2dcoder@naver.com>
2023-06-04 19:10:40 +0900

    delete: 새로운 프로젝트를 시작하기 위해 이전 코드 삭제

--- src/main/java/me/sully/springbootdeveloper/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package me.sully.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 엔티티 Member, 기본키 타입 Long
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
